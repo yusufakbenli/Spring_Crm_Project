4ef605db9fc8ba4790b7ca404d1418f7512c3637
yusufakbenli <akbenliyusuf@gmail.com>
2023-09-19 11:46:32 +0300

    Spring Crm CRUD aplication v1.0

---
+++ src/main/java/com/akbenli/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.akbenli.service;
+
+import java.util.List;
+
+import com.akbenli.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
